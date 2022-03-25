bff3800c4a31106be4a990fab4320a69e86d4894
AechGG <hazg@me.com>
2022-03-25 14:56:09 +0000

    add check for row win and some more constructor vars

--- src/Game.ts
+++ src/Game.ts
@@ -1,57 +1,84 @@
 export default class Game {
   points: number[][] = [];
 
-  constructor() {
-    this.points[0] = [0, 0, 0, 0, 0, 0];
-    this.points[1] = [0, 0, 0, 0, 0, 0];
-    this.points[2] = [0, 0, 0, 0, 0, 0];
-    this.points[3] = [0, 0, 0, 0, 0, 0];
-    this.points[4] = [0, 0, 0, 0, 0, 0];
-    this.points[5] = [0, 0, 0, 0, 0, 0];
-    this.points[6] = [0, 0, 0, 0, 0, 0];
+  players: number;
+
+  rows: number;
+
+  columns: number;
+
+  pointsInARowWin: number;
+
+  constructor(players: number, rows: number, columns: number, pointsInARowWin: number) {
+    this.players = players;
+    this.rows = rows;
+    this.columns = columns;
+    this.pointsInARowWin = pointsInARowWin;
+
+    this.points = new Array(rows);
+
+    for (let i = 0; i < rows; i++) {
+      this.points[i] = new Array(columns).fill(0);
+    }
   }
 
   playMove(player: number, column: number) {
-    if (column >= 0 && column < 6) {
-      for (let i = 6; i >= 0; i--) {
-        if (this.points[i][column] === 0) {
-          this.points[i][column] = player;
-          return;
-        }
+    const { success, row } = this.plotMove(player, column);
+
+    if (success) {
+      this.printGrid();
+      if (this.checkMoves(player, row, column)) {
+        console.log(`Player ${player} WINS`);
+      } else {
+        console.log('\nNext player turn\n');
       }
     }
   }
 
-  checkMoves(player: number) {
+  private plotMove(player: number, column: number): { success: boolean; row: number } {
+    if (column >= 0 && column < this.columns) {
+      for (let row = this.rows - 1; row >= 0; row--) {
+        if (this.points[row][column] === 0) {
+          this.points[row][column] = player;
+          return { success: true, row };
+        }
+      }
+    }
+    return { success: false, row: -1 };
+  }
+
+  checkMoves(player: number, row: number, column: number) {
     // row match would be x + 1 or x - 1
     // column match would be y + 1 or y - 1
     // diagonal would be (x + 1, y + 1) | (x - 1, y + 1) | (x + 1, y - 1) | (x - 1, y - 1)
 
-    let pointsInARow = [];
-    const pointsInAColumn = [];
-    const pointsInADiagional = [];
-    for (let y = 0; y < this.points.length; y++) {
-      for (let x = 0; x < this.points.length; x++) {
-        const currentPoint = this.points[y][x];
-        if (currentPoint === player) {
-          console.log(`player ${player} found at (${x},${y})`);
-          pointsInARow.push(player);
-        } else {
-          pointsInARow = [];
-        }
+    return this.checkRow(player, row, column);
+  }
 
-        console.log(pointsInARow);
-        if (pointsInARow.length === 4) {
-          console.log('Player has won');
-          return;
-        }
+  checkRow(player: number, row: number, column: number): boolean {
+    let inARow = [];
+    const startIndex = column - this.pointsInARowWin < 0 ? 0 : column - this.pointsInARowWin;
+    const endIndex = column + this.pointsInARowWin >= this.columns ? this.columns - 1 : column + this.pointsInARowWin;
+
+    for (let col = startIndex; col <= endIndex; col++) {
+      const currentPoint = this.points[row][col];
+
+      if (currentPoint === player) {
+        inARow.push(player);
+      } else {
+        inARow = [];
       }
+
+      if (inARow.length === this.pointsInARowWin) return true;
     }
+
+    return false;
   }
 
   printGrid() {
     for (let i = 0; i < this.points.length; i++) {
-      console.log(this.points[i]);
+      console.log('|', this.points[i].join(' | '), '|');
     }
+    console.log('-------------------------');
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,20 @@ import Game from './Game';
 
 console.log('hello world');
 
-const newGame = new Game();
+const newGame = new Game(2, 7, 6, 4);
 
-newGame.printGrid();
-
-console.log('new move');
 newGame.printGrid();
 
 newGame.playMove(1, 0);
-newGame.playMove(1, 5);
-newGame.playMove(1, 5);
-newGame.playMove(1, 5);
+newGame.playMove(1, 2);
+
+newGame.playMove(1, 4);
 newGame.playMove(1, 5);
 
-console.log('new move');
-newGame.printGrid();
+newGame.playMove(1, 3);
+
+// console.log('new move');
+// newGame.printGrid();
 
 // newGame.checkMoves(1);
 
